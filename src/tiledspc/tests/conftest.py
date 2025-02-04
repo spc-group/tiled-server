c77736901640688bdcde705ea8155eb55a1f2cff
Mark Wolfman <canismarko@gmail.com>
2025-02-04 10:41:31 -0600

    Updated test data to match new Tiled catalog structure.

--- src/tiledspc/tests/conftest.py
+++ src/tiledspc/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import pandas as pd
 import numpy as np
 from tiled.adapters.mapping import MapAdapter
-from tiled.adapters.xarray import DatasetAdapter
+from tiled.adapters.table import TableAdapter
 from tiled.client import Context, from_context
 from tiled.server.app import build_app
 
@@ -15,7 +15,7 @@ run1 = pd.DataFrame(
         "It_net_counts": np.abs(np.sin(np.linspace(0, 4 * np.pi, num=100))),
         "I0_net_counts": np.linspace(1, 2, num=100),
     }
-).to_xarray()
+)
 
 grid_scan = pd.DataFrame(
     {
@@ -24,7 +24,87 @@ grid_scan = pd.DataFrame(
         "aerotech_horiz": np.linspace(0, 104, num=105),
         "aerotech_vert": np.linspace(0, 104, num=105),
     }
-).to_xarray()
+)
+
+data_keys = {
+    "I0-mcs-scaler-channels-0-net_count": {
+        "dtype": "number",
+        "dtype_numpy": "<f8",
+        "limits": {
+            "control": {"high": 0.0, "low": 0.0},
+            "display": {"high": 0.0, "low": 0.0},
+        },
+        "object_name": "I0",
+        "precision": 0,
+        "shape": [],
+        "source": "ca://25idcVME:3820:scaler1_netA.A",
+        "units": "",
+    },
+    "I0-mcs-scaler-channels-3-net_count": {
+        "dtype": "number",
+        "dtype_numpy": "<f8",
+        "limits": {
+            "control": {"high": 0.0, "low": 0.0},
+            "display": {"high": 0.0, "low": 0.0},
+        },
+        "object_name": "I0",
+        "precision": 0,
+        "shape": [],
+        "source": "ca://25idcVME:3820:scaler1_netA.D",
+        "units": "",
+    },
+    "I0-mcs-scaler-elapsed_time": {
+        "dtype": "number",
+        "dtype_numpy": "<f8",
+        "limits": {
+            "control": {"high": 0.0, "low": 0.0},
+            "display": {"high": 0.0, "low": 0.0},
+        },
+        "object_name": "I0",
+        "precision": 3,
+        "shape": [],
+        "source": "ca://25idcVME:3820:scaler1.T",
+        "units": "",
+    },
+    "I0-net_current": {
+        "dtype": "number",
+        "dtype_numpy": "<f8",
+        "object_name": "I0",
+        "shape": [],
+        "source": "soft://I0-net_current(gain,count,clock_count,clock_frequency,counts_per_volt_second)",
+        "units": "A",
+    },
+    "ge_8element": {
+        "dtype": "array",
+        "dtype_numpy": "<u4",
+        "external": "STREAM:",
+        "object_name": "ge_8element",
+        "shape": [8, 4096],
+        "source": "ca://XSP_Ge_8elem:HDF1:FullFileName_RBV",
+    },
+    "ge_8element-element0-all_event": {
+        "dtype": "number",
+        "dtype_numpy": "<f8",
+        "external": "STREAM:",
+        "object_name": "ge_8element",
+        "shape": [],
+        "source": "ca://XSP_Ge_8elem:HDF1:FullFileName_RBV",
+    },
+    "sim_motor_2": {
+        "dtype": "number",
+        "dtype_numpy": "<f8",
+        "limits": {
+            "control": {"high": 32000.0, "low": -32000.0},
+            "display": {"high": 32000.0, "low": -32000.0},
+        },
+        "object_name": "sim_motor_2",
+        "precision": 5,
+        "shape": [],
+        "source": "ca://25idc:simMotor:m2.RBV",
+        "units": "degrees",
+    },
+}
+
 
 hints = {
     "energy": {"fields": ["energy_energy", "energy_id_energy_readback"]},
@@ -35,9 +115,12 @@ bluesky_mapping = {
         {
             "primary": MapAdapter(
                 {
-                    "data": DatasetAdapter.from_dataset(run1),
+                    "internal": MapAdapter(
+                        {
+                            "events": TableAdapter.from_pandas(run1)
+                        }),
                 },
-                metadata={"descriptors": [{"hints": hints}]},
+                metadata={"hints": hints, "data_keys": data_keys},
             ),
         },
         metadata={
@@ -53,9 +136,11 @@ bluesky_mapping = {
         {
             "primary": MapAdapter(
                 {
-                    "data": DatasetAdapter.from_dataset(run1),
+                    "internal": MapAdapter({
+                        "events": TableAdapter.from_pandas(run1),
+                    }),
                 },
-                metadata={"descriptors": [{"hints": hints}]},
+                metadata={"hints": hints},
             ),
         },
         metadata={
@@ -71,7 +156,10 @@ bluesky_mapping = {
         {
             "primary": MapAdapter(
                 {
-                    "data": DatasetAdapter.from_dataset(grid_scan),
+                    "internal": MapAdapter(
+                        {
+                            "events": TableAdapter.from_pandas(grid_scan),
+                        }),
                 },
                 metadata={
                     "descriptors": [
