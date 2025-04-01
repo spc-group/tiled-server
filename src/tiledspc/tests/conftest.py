12346c4cd1e7a861850c02242dad277d44e09c25
Mark Wolfman <canismarko@gmail.com>
2025-04-01 14:50:16 -0500

    Removed stale unused test data.

--- src/tiledspc/tests/conftest.py
+++ src/tiledspc/tests/conftest.py
@@ -148,27 +148,10 @@ data_keys = {
         "shape": [],
         "source": "ca://XSP_Ge_8elem:HDF1:FullFileName_RBV",
     },
-    # "sim_motor_2": {
-    #     "dtype": "number",
-    #     "dtype_numpy": "<f8",
-    #     "limits": {
-    #         "control": {"high": 32000.0, "low": -32000.0},
-    #         "display": {"high": 32000.0, "low": -32000.0},
-    #     },
-    #     "object_name": "sim_motor_2",
-    #     "precision": 5,
-    #     "shape": [],
-    #     "source": "ca://25idc:simMotor:m2.RBV",
-    #     "units": "degrees",
-    # },
 }
 
 
 baseline_data_keys = {
-    # "aps_current": np.asarray([130.0, 204.1]),
-    #     "aps_fill_number": np.asarray([1, 2]),
-    #     "aps_global_feedback": np.asarray([True, False]),
-    # }
     "aps_current": {
         "dtype": "number",
         "dtype_numpy": "<f8",
@@ -212,120 +195,6 @@ hints = {
     "It": {"fields": ["It-net_current"]},
     "ge_8element": {"fields": ["ge_8element"]},
 }
-
-bluesky_mapping = {
-    # "7d1daf1d-60c7-4aa7-a668-d1cd97e5335f": MapAdapter(
-    #     {
-    #         "primary": MapAdapter(
-    #             {
-    #                 "internal": MapAdapter({"events": TableAdapter.from_pandas(run1)}),
-    #                 "config": MapAdapter(
-    #                     {
-    #                         "energy": TableAdapter.from_pandas(
-    #                             pd.DataFrame(
-    #                                 {
-    #                                     "energy-monochromator-d_spacing": [3.13],
-    #                                 }
-    #                             )
-    #                         ),
-    #                     }
-    #                 ),
-    #             },
-    #             metadata={"hints": hints, "data_keys": data_keys},
-    #         ),
-    #     },
-    #     metadata={
-    #         "plan_name": "xafs_scan",
-    #         "start": {
-    #             "plan_name": "xafs_scan",
-    #             "uid": "7d1daf1d-60c7-4aa7-a668-d1cd97e5335f",
-    #             "hints": {"dimensions": [[["energy"], "primary"]]},
-    #         },
-    #     },
-    # ),
-    # "9d33bf66-9701-4ee3-90f4-3be730bc226c": MapAdapter(
-    #     {
-    #         "primary": MapAdapter(
-    #             {
-    #                 "internal": MapAdapter(
-    #                     {
-    #                         "events": TableAdapter.from_pandas(run1),
-    #                     }
-    #                 ),
-    #             },
-    #             metadata={"hints": hints},
-    #         ),
-    #     },
-    #     metadata={
-    #         "start": {
-    #             "plan_name": "rel_scan",
-    #             "uid": "9d33bf66-9701-4ee3-90f4-3be730bc226c",
-    #             "hints": {"dimensions": [[["pitch2"], "primary"]]},
-    #         }
-    #     },
-    # ),
-    # 2D grid scan map data
-    "85573831-f4b4-4f64-b613-a6007bf03a8d": MapAdapter(
-        {
-            "primary": MapAdapter(
-                {
-                    "internal": MapAdapter(
-                        {
-                            "events": TableAdapter.from_pandas(grid_scan),
-                        }
-                    ),
-                },
-                metadata={
-                    "descriptors": [
-                        {
-                            "hints": {
-                                "Ipreslit": {"fields": ["Ipreslit_net_counts"]},
-                                "CdnIPreKb": {"fields": ["CdnIPreKb_net_counts"]},
-                                "I0": {"fields": ["I0_net_counts"]},
-                                "CdnIt": {"fields": ["CdnIt_net_counts"]},
-                                "aerotech_vert": {"fields": ["aerotech_vert"]},
-                                "aerotech_horiz": {"fields": ["aerotech_horiz"]},
-                                "Ipre_KB": {"fields": ["Ipre_KB_net_counts"]},
-                                "CdnI0": {"fields": ["CdnI0_net_counts"]},
-                                "It": {"fields": ["It_net_counts"]},
-                            }
-                        }
-                    ]
-                },
-            ),
-        },
-        metadata={
-            "start": {
-                "plan_name": "grid_scan",
-                "uid": "85573831-f4b4-4f64-b613-a6007bf03a8d",
-                "hints": {
-                    "dimensions": [
-                        [["aerotech_vert"], "primary"],
-                        [["aerotech_horiz"], "primary"],
-                    ],
-                    "gridding": "rectilinear",
-                },
-                "shape": [5, 21],
-                "extents": [[-80, 80], [-100, 100]],
-            },
-        },
-    ),
-}
-
-
-mapping = {
-    "255id_testing": MapAdapter(bluesky_mapping),
-}
-
-tree = MapAdapter(mapping)
-
-
-@pytest.fixture(scope="session")
-def tiled_client():
-    app = build_app(tree)
-    with Context.from_app(app) as context:
-        client = from_context(context)
-        yield client["255id_testing"]
 
 
 @pytest.fixture
